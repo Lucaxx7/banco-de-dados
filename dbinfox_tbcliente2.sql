f3a2b32ead298ddc5481f8c40fe5bbcb7e484fd5
Lucas Souza Oliveira <38111159+lucassouzaolivei@users.noreply.github.com>
2018-07-25 16:17:52 -0300

    atividade_dbinfoxtb_cliente2
    25/07

---
+++ dbinfox_tbcliente2.sql
@@ -0,0 +1,82 @@
+-- CRUD (Create Read Update Delete)
+use dbinfox;
+-- inspecionar tabelas no banco de dados
+show tables;
+-- o código abaixo cria uma tabela
+create table tb_usuarios(
+iduser int primary key,
+usuario varchar(50) not null,
+fone varchar(15) not null,
+login varchar (50) not null unique,
+senha varchar (50) not null,
+perfil varchar (50) not null
+);
+-- descrevendo a tabela
+describe tb_usuarios;
+-- inserindo dados na tabela (CRUD - Create)
+insert into tb_usuarios values
+(1, 'Lucas Souza Oliveira','1111-1111','lucas1','123@senac','Aluno');
+insert into tb_usuarios values
+(2, 'Paulo Vítor','2222-2222','paulo.vitor1','paulo123','Segurança');
+insert into tb_usuarios values
+(3, 'Sabrina Silva','3333-3333','sabrina.silva35','sabrina123','Aluno');
+select * from tb_usuarios;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+-- CRUD (Create Read Update Delete)
+use dbinfox;
+-- inspecionar tabelas no banco de dados
+show tables;
+-- o código abaixo cria uma tabela
+create table tb_cliente(
+idcli int auto_increment primary key,
+cpfcli varchar(50) unique,
+nomecli varchar (50) not null,
+cep varchar(15) not null,
+tipo varchar (50) not null,
+logradouro varchar (50) not null,
+numero varchar (50) not null,
+complemento varchar (50),
+bairro varchar (50) not null,
+cidade varchar (50) not null,
+uf char (2) not null,
+fone1 varchar (50) not null,
+fone2 varchar (50),
+emailcli varchar (50) not null
+);
+describe tb_cliente;
+-- inserindo dados na tabela (CRUD - Create)
+insert into tb_cliente values
+(1, '123.456.789-00','César Silva','01234-56','Rua','Rua Cássio Álvarez','33','','Campo Belo','São Paulo','SP','98765-4321','','cesarsil@gmail.com');
+select * from tb_cliente;
+
+delete from tb_cliente where idcli=1;
+insert into tb_cliente values
+(null, '123.456.788-00','César Silva','01230-56','Rua','Rua Pedro José','33','','Campo Belo','São Paulo','SP','98765-4321','','pedro@gmail.com');
+
+insert into tb_cliente values
+(null, '222.256.788-00','João Carlos','01637-00','Avenida','AV. Cupecê','33','','Américanópolis','São Paulo','SP','97700-4021','','joao@gmail.com');
+
+insert into tb_cliente values
+(null, '232.209.009-00','Caio Alves','88637-50','Rua','Rua Marta Aparecida','1222','','Vila Carrão','São Paulo','SP','95408-4561','','caio@outlook.com');
+
+insert into tb_cliente values
+(null, '657.298.600-32','Lucas Castro','00933-46','Avenida','AV. Cupecê','33','','Morumbi','São Paulo','SP','96765-1245','','lucas@gmail.com');
+
+insert into tb_cliente values
+(null, '211.168.776-12','Roberto Brandão','13243-66','Rua','Rua João Dias','45','','Itaquera','São Paulo','SP','95785-9200','','roberto@bol.com.br');
+
